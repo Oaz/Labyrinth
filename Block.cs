53e3c5564ff5ec190d0bf193e42d946bc5f075cd
Oaz <github@azeau.com>
2011-04-20 12:17:51 +0200

    first commit

---
+++ Block.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Labyrinth
+{
+	public class Walls
+  {
+    public const int Nothing = 0;
+    public const int Top = 1;
+    public const int Bottom = 2;
+    public const int Right = 4;
+    public const int Left = 8;
+  }
+
+	public class Block
+	{
+    public int Closed { get; set; }
+
+    public static Block From(string blockDefinition)
+    {
+      return new Block();
+    }
+
+    public override bool Equals(object otherBlock)
+    {
+       return Closed == ((Block)otherBlock).Closed;
+    }
+
+    public override string ToString()
+    {
+       var char1 = (Closed == Walls.Left) ? "(" : " ";
+       return char1 + "  ";
+    }
+	}
+}
+
+
---
+++ TestNotation.cs
@@ -0,0 +1,19 @@
+using System;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace Labyrinth.Tests
+{
+	[TestFixture]
+	public class TestDisplay
+	{
+		[Test]
+		public void ToSimpleNotation()
+		{
+			Assert.That( new Block { Closed = Walls.Nothing }.ToString(), Is.EqualTo("   ") );
+			Assert.That( new Block { Closed = Walls.Left }.ToString(),    Is.EqualTo("(  ") );
+		}
+  }
+}
+
+
