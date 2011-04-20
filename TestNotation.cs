c719ffe86e5c4e61d946674ebaff97122f0b47bf
Oaz <github@azeau.com>
2011-04-20 12:27:39 +0200

    ToSimpleNotation - TopBottom

--- TestNotation.cs
+++ TestNotation.cs
@@ -10,11 +10,12 @@ namespace Labyrinth.Tests
 		[Test]
 		public void ToSimpleNotation()
 		{
-			Assert.That( new Block { Closed = Walls.Nothing }.ToString(), Is.EqualTo("   ") );
-			Assert.That( new Block { Closed = Walls.Left }.ToString(),    Is.EqualTo("(  ") );
-			Assert.That( new Block { Closed = Walls.Right }.ToString(),   Is.EqualTo("  )") );
-			Assert.That( new Block { Closed = Walls.Bottom }.ToString(),  Is.EqualTo(" _ ") );
-			Assert.That( new Block { Closed = Walls.Top }.ToString(),     Is.EqualTo(" ^ ") );
+			Assert.That( new Block { Closed = Walls.Nothing }.ToString(),                Is.EqualTo("   ") );
+			Assert.That( new Block { Closed = Walls.Left }.ToString(),                   Is.EqualTo("(  ") );
+			Assert.That( new Block { Closed = Walls.Right }.ToString(),                  Is.EqualTo("  )") );
+			Assert.That( new Block { Closed = Walls.Bottom }.ToString(),                 Is.EqualTo(" _ ") );
+			Assert.That( new Block { Closed = Walls.Top }.ToString(),                    Is.EqualTo(" ^ ") );
+			Assert.That( new Block { Closed = Walls.Top + Walls.Bottom }.ToString(),     Is.EqualTo(" O ") );
 		}
   }
 }
